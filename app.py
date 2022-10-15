9f0275da49e157b6904f06f617cf4b98fb9a6082
Romana Hussain <rohussain@microsoft.com>
2022-10-15 11:47:43 +0530

    implementing anime api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, request
 import requests
 app = Flask(__name__)
 app.debug = True
@@ -8,15 +8,15 @@ app.debug = True
 def hello():
     return '{"Student Number": "200530943", "Student Name":"Romana Hussain"}'
 
-@app.route('/webhook',methods=['GET'])
+@app.route('/webhook',methods=['POST'])
 def index():
+
+    body = request.json
+    title= body['title']
+
     #Connect to the API anf get the JSON file.
-    api_url='https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,explicit'
+    api_url='https://animechan.vercel.app/api/quotes/anime?title=' + title
     headers = {'Content-Type': 'application/json'} #Set the HTTP header for the API request
     response = requests.get(api_url, headers=headers) #Connect to openweather and read the JSON response.
     r=response.json() #Conver the JSON string to a dict for easier parsing.
-
-    if "joke" in r:
-        return  "Joke: " + str(r['joke'])
-    else:
-        return "Setup: " + str(r['setup'])  + "Punch:" + str(r['delivery'])
+    return r[0]
