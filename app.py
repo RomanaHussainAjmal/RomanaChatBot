7da7772a874bfd405fb5fc2d7a2f548c83807791
Romana Hussain <rohussain@microsoft.com>
2022-10-15 13:29:36 +0530

    reply formatting

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def index():
     headers = {'Content-Type': 'application/json'} #Set the HTTP header for the API request
     response = requests.get(api_url, headers=headers) #Connect to openweather and read the JSON response.
     r=response.json() #Conver the JSON string to a dict for easier parsing.
-    return '{"fulfillmentMessages": [ {"text": {"text": \'' + r[0]['quote'] + ' \'} } ]}'
+    return '{"fulfillmentMessages": [ {"text": {"text": \"' + r[0]['quote'] + ' \"} } ]}'
