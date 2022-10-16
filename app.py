4a5ead7d09e8f77a4b2bb46a0c3921889f8b951b
Romana Hussain <rohussain@microsoft.com>
2022-10-16 11:53:00 +0530

    input

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ import requests
 app = Flask(__name__)
 app.debug = True
 
+
+def apiCall(AnimeShowName):
+    #Connect to the API anf get the JSON file.
+    api_url='https://animechan.vercel.app/api/quotes/anime?title=' + AnimeShowName
+    headers = {'Content-Type': 'application/json'} #Set the HTTP header for the API request
+    response = requests.get(api_url, headers=headers) #Connect to openweather and read the JSON response.
+    r=response.json() #Conver the JSON string to a dict for easier parsing.
+    return '{"fulfillmentMessages": [ {"text": {"text": [\"' + r[0]['quote'] + ' \"]} } ]}'
+
 @app.route('/')
 def hello():
     return '{"Student Number": "200530943", "Student Name":"Romana Hussain"}'
@@ -12,11 +21,21 @@ def hello():
 def index():
 
     body = request.json
-    AnimeShowName= body['queryResult']['parameters']['AnimeShowName']
 
-    #Connect to the API anf get the JSON file.
-    api_url='https://animechan.vercel.app/api/quotes/anime?title=' + AnimeShowName
-    headers = {'Content-Type': 'application/json'} #Set the HTTP header for the API request
-    response = requests.get(api_url, headers=headers) #Connect to openweather and read the JSON response.
-    r=response.json() #Conver the JSON string to a dict for easier parsing.
-    return '{"fulfillmentMessages": [ {"text": {"text": [\"' + r[0]['quote'] + ' \"]} } ]}'
+    if(body['queryResult']['parameters']['AnimeShowName'].length!=0):
+        apiCall(body['queryResult']['parameters']['AnimeShowName'])
+
+    elif (body['queryResult']['parameters']['AnimeShowName1'].length!=0):
+        apiCall(body['queryResult']['parameters']['AnimeShowName1'])
+
+    elif (body['queryResult']['parameters']['AnimeShowName2'].length!=0):
+        apiCall(body['queryResult']['parameters']['AnimeShowName2'])
+
+    elif (body['queryResult']['parameters']['AnimeShowName3'].length!=0):
+        apiCall(body['queryResult']['parameters']['AnimeShowName3'])
+
+    elif (body['queryResult']['parameters']['AnimeShowName4'].length!=0):
+        apiCall(body['queryResult']['parameters']['AnimeShowName4'])
+    
+    else :
+        apiCall("")
